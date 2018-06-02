1b1e3458c4c173b052b321720af6cfbc8d25377e
ajay <ajkn1992@gmail.com>
2018-06-02 22:28:50 +0530

    Slash

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<category>/<slug>', views.post_detail, name='post_detail'),
-    path('category/posts/<name>', views.cat_detail, name='cat_detail'),
+    path('<category>/<slug>/', views.post_detail, name='post_detail'),
+    path('category/posts/<name>/', views.cat_detail, name='cat_detail'),
 ]
